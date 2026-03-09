f3ecc457d6f36fa45af7c9d82569fb0330e2651b
ingridmoitinho <ingridmoitinho@gmail.com>
2026-03-09 12:29:06 -0300

    feat: add exe002 (input nome), exe003 (soma de números) e exe004 (análise de string)

---
+++ python-exercicios-m1-curso-em-video/exe003.py
@@ -0,0 +1,5 @@
+numero1 = int(input('Digite um número:'))
+numero2 = int(input('Digite mais um número:'))
+soma = numero1 + numero2
+# print('A soma vale' , soma )] sintaxe antiga
+print('A soma entre {} e {} é igual a {}!' .format(numero1, numero2, soma))
